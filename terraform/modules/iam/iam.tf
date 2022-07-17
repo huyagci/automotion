91e8e8d6e551044e5d4a4d2018b05f1d3f2224fa
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-07-17 14:57:58 +0300

    Import aws service linked role for auto scaling

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -1,4 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+
+# Required for ECS Task Definition 
 data "aws_iam_role" "ecs_task_exec_role" {
   name = "ecsTaskExecutionRole"
+}
+
+# Required for Application Auto Scaling
+resource "aws_iam_service_linked_role" "ecs_app_auto_scale_sl_role" {
+  aws_service_name = "ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService"
 }
--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,3 +1,7 @@
 output "ECS_TASK_EXEC_ROLE_ARN" {
   value = data.aws_iam_role.ecs_task_exec_role.arn
 }
+
+output "ECS_APP_AUTO_SCALE_SL_ROLE_ARN" {
+  value = aws_iam_service_linked_role.ecs_app_auto_scale_sl_role.arn
+}
