d8a7a63b065d299f0d654e7e4a5f2915af33b3dd
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-07-18 03:21:07 +0300

    Enable container insights for ecs cluster

--- terraform/modules/ecs/clusters.tf
+++ terraform/modules/ecs/clusters.tf
@@ -1,6 +1,11 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster
 resource "aws_ecs_cluster" "cluster" {
   name = "p-app-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster_capacity_providers
